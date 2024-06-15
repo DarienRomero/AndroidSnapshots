fc485fb2c5725da0978a3696fc28338e82727cdb
Darien Romero <darien.r.leiva@gmail.com>
2024-06-15 17:09:51 -0500

    avances

--- app/src/main/java/com/example/snapshots/Snapshot.kt
+++ app/src/main/java/com/example/snapshots/Snapshot.kt
@@ -3,4 +3,9 @@ package com.example.snapshots
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-data class Snapshot(var id: String, var title: String, var photoUrl: String = "", var likeList: Map<String,Boolean> = mutableMapOf())
+data class Snapshot(
+    var id: String = "",
+    var title: String = "",
+    var photoUrl: String = "",
+    var likeList: Map<String,Boolean> = mutableMapOf()
+)
